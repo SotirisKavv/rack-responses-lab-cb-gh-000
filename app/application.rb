abe269e2811c0415a98c62218f02af3d42d7514f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-21 19:39:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     if Time.now.hour < 12
-      resp.write "Good Morning"
+      resp.write "Good Morning!"
     else
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     end
   end
 end
